cd966bb6abe63253d6aa66fce6c52b8f08407097
jokubaslink <jokubaslink@gmail.com>
2023-02-09 21:56:46 +0200

    added favourite movies list, fixing bugs.

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -26,22 +26,25 @@ function Page() {
     })
   })
 
-  function likeMovie(){
-    const movie ={
-      title: movieId,
-      term: searchTerm,
-      user: user,
-    };
-    addDoc(collection(db, "likes"), movie)
-    console.log(movie)
-    console.log('great success!!')
-  }
+ function likeMovie(){
+      const movie ={
+        user: user,
+        movieId: movieId,
+        searchTerm: searchTerm,
+        movieTitle: video.Title,
+        moviePoster: video.Poster,
+      };
+      addDoc(collection(db, "likes"), movie)
+      console.log(movie)
+      console.log('great success!!')
+    }
 
   async function fetchMovie() {
     const { data } = await axios.get(
       `https://www.omdbapi.com/?apikey=eeef1900&i=${movieId}`
     );
     getVideo(data);
+    console.log(data)
     setLoading(false);
   }
 
@@ -125,7 +128,10 @@ function Page() {
               </div>
             </>
           )}
-          <button className="likeButton" onClick={likeMovie}>
+          {
+            user.length > 0 && (<button className="likeButton" onClick={() => {
+              likeMovie()
+            }}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -140,7 +146,8 @@ function Page() {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
-          </button>
+          </button>)
+          }
         </div>
 
         <div className="borderSpacing"></div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
 import Nav from "../components/Nav";
 import "./Profile.css";
-import {collection, getDocs, query, where} from "firebase/firestore";
-import {db, auth} from '../firebase/init'
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db, auth } from "../firebase/init";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-  const [deita, setDeita] = useState([]);
-  const [user, setUser] = useState('');
-  const [userEmail, setUserEmail] = useState('');
+  const [user, setUser] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      if(user){
-        setUser(user.uid)
-        setUserEmail(user.email)
-        console.log(user)
+      if (user) {
+        setUser(user.uid);
+        setUserEmail(user.email);
+        console.log(user);
       }
-    })
-    async function getLikes(){
+    });
+  }, []);
+
+  useEffect(() => {
+    async function getLikes() {
       const likeCollectionRef = await query(
         collection(db, "likes"),
         where("user", "==", user)
-      )
-      const {docs} = await getDocs(likeCollectionRef);
-      const data = docs.map(doc => doc.data())
-      console.log(data)
+      );
+      const { docs } = await getDocs(likeCollectionRef);
+      const data = docs.map((doc) => doc.data());
+      if (data.length > 0) {
+        setMovies(data);
+        console.log(movies);
+      }
     }
-    getLikes()
-  }, [])
+    getLikes();
+  }, [user.length > 0]);
 
   return (
     <div className="container">
@@ -43,6 +50,21 @@ function Profile() {
             <div className="spacing"></div>
             <h3>Favourite Movies:</h3>
             <div className="favouriteMovies">
+              {movies.length > 0 ? (movies.map((movie) => (
+                <div
+                  className="favouriteMovie" onClick={() => navigate(`/${movie.searchTerm}/${movie.movieId}`)}
+                >
+                  <img
+                    className="favouriteMovie--img"
+                    src={movie.moviePoster}
+                    alt=""
+                  />
+                  <h3 className="favouriteMovie--title">{movie.movieTitle}</h3>
+                </div>
+              ))) : (
+                <h2>There's no favourite movies!</h2>
+              )
+              }
             </div>
           </div>
         </div>
